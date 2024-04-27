2911ef01640487f9f02f239ce75da4541314cd25
Henk van Kampen <henkvk@gmail.com>
2024-04-27 15:45:01 +0200

    Update readme: table format change

---
+++ src/test/java/com/hbvk/jep454/Jep454ForeignFunctionsTest.java
@@ -0,0 +1,49 @@
+package com.hbvk.jep454;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Jep454ForeignFunctionsTest {
+
+    final Linker linker = Linker.nativeLinker();
+    final SymbolLookup stdlib = linker.defaultLookup();
+
+    @Test
+    void testCallStrlen() throws Throwable {
+        final var helloWorld = "Hello world!";
+        MethodHandle strlen = linker
+                .downcallHandle(
+                        stdlib.find("strlen").orElseThrow(),
+                        FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS));
+        try (Arena offHeap = Arena.ofConfined()) {
+            MemorySegment str = offHeap.allocateFrom(helloWorld);
+            assertEquals((long) helloWorld.length(), strlen.invoke(str));
+        }
+    }
+
+    @Test
+    void testCallIslower() throws Throwable {
+        MethodHandle islower = linker
+                .downcallHandle(
+                        stdlib.find("islower").orElseThrow(),
+                        FunctionDescriptor.of(ValueLayout.JAVA_BOOLEAN, ValueLayout.JAVA_CHAR));
+        assertEquals(Boolean.TRUE, islower.invoke('l'));
+        assertEquals(Boolean.FALSE, islower.invoke('L'));
+        assertEquals(Boolean.FALSE, islower.invoke('7'));
+    }
+
+    @Test
+    void testCallIsdigit() throws Throwable {
+        MethodHandle isdigit = linker
+                .downcallHandle(
+                        stdlib.find("isdigit").orElseThrow(),
+                        FunctionDescriptor.of(ValueLayout.JAVA_BOOLEAN, ValueLayout.JAVA_CHAR));
+        assertEquals(Boolean.FALSE, isdigit.invoke('l'));
+        assertEquals(Boolean.FALSE, isdigit.invoke('L'));
+        assertEquals(Boolean.TRUE, isdigit.invoke('7'));
+    }
+}
